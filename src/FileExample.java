1ab9620d3f014e865924d7dd7923a3558d473756
Lornst <augustgosden@gmail.com>
2025-04-03 21:31:01 +0200

    new class

--- src/FileExample.java
+++ src/FileExample.java
@@ -1,2 +1,15 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileExample {
+    public static void main(String[] args) throws IOException {
+        File myFile = new File("data.txt");
+        FileWriter writer = new FileWriter("data.txt");
+
+        writer.write("Hej, dette er en test.");
+        writer.write("Her er der en til sætning.");
+
+        writer.close();
+    }
 }
